f637d9b344fc83b91fb9c4433e9bed660d7f6352
Arthurwiz <Arthurwiz@users.noreply.github.com>
2020-06-02 21:54:26 -0700

    Add files via upload

---
+++ Course05/myjs.js
@@ -0,0 +1,19 @@
+﻿function validateuserinputs() {
+    
+    var FirstName = document.getElementById("FirstName");
+    var LastName = document.getElementById("LastName");
+    var product = document.getElementById("product");
+    var txtproblem = document.getElementById("txtproblem");
+
+    if (txtproblem.value == '' || FirstName.value == '' || LastName.value == '' || product.value == '') {
+        alert("Please fill out the required fields.");
+    }
+    else {
+        submitdata();
+    };
+}
+
+function submitdata() {
+    //code for submitting to an API
+    alert("The ticket has been successfully created.");
+}
